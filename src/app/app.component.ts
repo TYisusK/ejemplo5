8db88fd747c47d7c819fb1d04976832df54f12ba
“TYisusK” <jesussc.ti22@utsjr.edu.mx>
2025-02-01 22:59:38 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,57 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ejemplo5';
+  title = 'Ejemplo5';
+
+  //Propiedades.
+  nombreCliente = '';
+  emailCliente = '';
+  mensajeCliente = '';
+  statusCliente = '';
+  municipioCliente = '';
+  autorizacionCliente = false; 
+
+  enviado = false;
+
+  formContacto = new FormGroup({
+    nombre: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    mensaje: new FormControl('',[
+      Validators.required,
+      Validators.maxLength(500),
+    ]),
+    status: new FormControl('',[
+      Validators.required,
+    ]),
+    municipio: new FormControl(''),
+    autorizacion: new FormControl(false),
+  });
+
+  //Método que se ejecuta cuando se envia el formulario.
+  onSubmit(){
+    this.enviado = true;
+    if(this.formContacto.valid){
+      this.nombreCliente = this.formContacto.value.nombre!;
+      this.emailCliente = this.formContacto.value.email!;
+      this.mensajeCliente = this.formContacto.value.mensaje!;
+      this.statusCliente = this.formContacto.value.status!;
+      this.municipioCliente = this.formContacto.value.municipio!;
+      this.autorizacionCliente = this.formContacto.value.autorizacion!;
+    }
+   
+  }
 }
